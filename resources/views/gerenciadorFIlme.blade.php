5a45b97220ecdf682701705903d76f2df73d441a
maxx0501 <mateused0501@gmail.com>
2023-05-07 13:24:14 -0300

    excluir filme pronto

--- resources/views/gerenciadorFIlme.blade.php
+++ resources/views/gerenciadorFIlme.blade.php
@@ -18,12 +18,21 @@
 </div>
 <div class="container my-3">
     <table class="table table-dark table-hover">
+    <thead>
+        <tr>
+        <th scope="col">Código</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Atores</th>
+        <th scope="col">Alterar</th>
+        <th scope="col">Excluir</th>
+        </tr>
+    </thead>
     <tbody>
     @foreach($dadosFilme as $dadosFilmes)
         <tr>
-        <td scope="col">Código</td>
-        <td scope="col">Nome</td>
-        <td scope="col">Atores</td>
+        <td scope="row">{{$dadosFilmes->id}}</td>
+        <td>{{$dadosFilmes->nomefilme}}</td>
+        <td>{{$dadosFilmes->atoresfilme}}</td>
         <td><a href="{{route('mostrar-filme', $dadosFilmes->id)}}">Alterar</a></td>
         <td>
             <form method="post" action="{{route('apagar-filme', $dadosFilmes->id)}}">
@@ -32,22 +41,22 @@
             <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal"> Excluir </button>
             <form>
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-        <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            Tem certeza que deseja excluir?
-        </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-        <button type="submit" class="btn btn-primary">Salvar</button>
-      </div>
-    </div>
-  </div>
-</div>
+              <div class="modal-dialog">
+                  <div class="modal-content bg-dark">
+                  <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">Atenção</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                      Tem certeza que deseja excluir?
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                    <button type="submit" class="btn btn-primary">Salvar</button>
+                  </div>
+                </div>
+              </div>
+            </div>
         </tr>
     </tbody>
     @endforeach
--- resources/views/gerenciadorFuncionario.blade.php
+++ resources/views/gerenciadorFuncionario.blade.php
@@ -44,7 +44,7 @@
             <form>
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content bg-dark">
         <div class="modal-header">
             <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
