0728ab95b2e437ce4170f06fb59dc927f75a5c18
unknown <thiaguinhoLrodrigues007@gmail.com>
2023-05-24 22:30:30 -0300

    front end tentativa

--- resources/views/padrao.blade.php
+++ resources/views/padrao.blade.php
@@ -9,56 +9,66 @@
     <title>Cinema Adm</title>
 </head>
 <body>
-<header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Tenth navbar example">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-    
-          <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{url('/home')}}">Cinema da Etec</a>
-              </li>
-              
-              
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Filme</a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="{{url('/cadastro-filme')}}">Cadastrar</a></li>
-                  <li><a class="dropdown-item" href="{{url('gerenciar-filme')}}">Gerenciar</a></li>
-                </ul>
-              </li>
 
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Funcionário</a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="{{url('/cadastro-funcionario')}}">Cadastrar</a></li>
-                    <li><a class="dropdown-item" href="{{url('gerenciar-funcionario')}}">Gerenciar</a></li>
-                </ul>
-              </li>
-
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Sala/Poltrona</a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="{{url('/cadastro-poltrona')}}">Cadastrar</a></li>
-                    <li><a class="dropdown-item" href="{{url('/gerenciar-poltrona')}}">Gerenciar</a></li>
-                </ul>
-              </li>
-            </ul>
-          </div>
+     <!-- Header Section Begin -->
+     <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2">
+                    <div class="header__logo">
+                        <a href="./index.html">
+                            <img src="img/logo-cinemark.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="header__nav">
+                        <nav class="header__menu mobile-menu">
+                            <ul>
+                                <li class="active"><a href="./index.html">Programação <span
+                                            class="arrow_carrot-down"></span></a>
+                                    <ul class="dropdown">
+                                        <li><a href="#emCartaz">Em Cartaz</a></li>
+                                        <li><a href="#emBreve">Em Breve</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="./categories.html">Snack Bar <span class="arrow_carrot-down"></span></a>
+                                    <ul class="dropdown">
+                                        <li><a href="#">Cardápio Snack Bar</a></li>
+                                        <li><a href="#">Cardápio Prime</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="./categories.html">Corporativo<span class="arrow_carrot-down"></span></a>
+                                    <ul class="dropdown">
+                                        <li><a href="#">Aluguel de Salas</a></li>
+                                        <li><a href="#">Ingressos Corporativos</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Fidelidade</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="header__right">
+                        <a href="#" class="search-switch"><span class="icon_search"></span></a>
+                        <a href="./login.html"><span class="icon_profile"></span></a>
+                    </div>
+                </div>
+            </div>
+            <div id="mobile-menu-wrap"></div>
         </div>
-      </nav>
-    
-<div class="container">
+    </header>
+    <!-- Header End -->
 
-@yield('content')
+
 
 </div>
 
     </header>
+    @yield('content')
 
+    
     <footer>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>   
