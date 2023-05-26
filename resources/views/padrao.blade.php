22b1c932341d1e5e670baeb7e7c6b396273b2359
Maxx0501 <mateused0501@gmail.com>
2023-05-25 22:44:13 -0300

    front quase pronto

--- resources/views/padrao.blade.php
+++ resources/views/padrao.blade.php
@@ -4,7 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/elegant-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/nice-select.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/sicknav.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/plyr.css')}}">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Cinema Adm</title>
 </head>
@@ -25,23 +38,23 @@
                     <div class="header__nav">
                         <nav class="header__menu mobile-menu">
                             <ul>
-                                <li class="active"><a href="./index.html">Programação <span
+                                <li class="active"><a href="./index.html">Filme <span
                                             class="arrow_carrot-down"></span></a>
                                     <ul class="dropdown">
-                                        <li><a href="#emCartaz">Em Cartaz</a></li>
-                                        <li><a href="#emBreve">Em Breve</a></li>
+                                    <li><a class="dropdown-item" href="{{url('/cadastro-filme')}}">Cadastrar</a></li>
+                                    <li><a class="dropdown-item" href="{{'/gerenciar-filme'}}">Gerenciar</a></li>
                                     </ul>
                                 </li>
-                                <li><a href="./categories.html">Snack Bar <span class="arrow_carrot-down"></span></a>
+                                <li><a href="./categories.html">Funcionario <span class="arrow_carrot-down"></span></a>
                                     <ul class="dropdown">
-                                        <li><a href="#">Cardápio Snack Bar</a></li>
-                                        <li><a href="#">Cardápio Prime</a></li>
+                                    <li><a class="dropdown-item" href="{{url('/cadastro-funcionario')}}">Cadastrar</a></li>
+                                    <li><a class="dropdown-item" href="{{'/gerenciar-funcionario'}}">Gerenciar</a></li>
                                     </ul>
                                 </li>
-                                <li><a href="./categories.html">Corporativo<span class="arrow_carrot-down"></span></a>
+                                <li><a href="./categories.html">Poltrona<span class="arrow_carrot-down"></span></a>
                                     <ul class="dropdown">
-                                        <li><a href="#">Aluguel de Salas</a></li>
-                                        <li><a href="#">Ingressos Corporativos</a></li>
+                                    <li><a class="dropdown-item" href="{{url('/cadastro-poltrona')}}">Cadastrar</a></li>
+                                    <li><a class="dropdown-item" href="{{'/gerenciar-poltrona'}}">Gerenciar</a></li>
                                     </ul>
                                 </li>
                                 <li><a href="#">Fidelidade</a></li>
@@ -70,10 +83,19 @@
 
     
     <footer>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+   
+
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>   
     <script src="{{asset('js/script.js') }}"></script>
-</footer>
+    <script src="{{asset('js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{asset('js/player.js') }}"></script>
+    <script src="{{asset('js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{asset('js/mixitup.min.js') }}"></script>
+    <script src="{{asset('js/jquery.slicknav.js') }}"></script>
+    <script src="{{asset('js/owl.carousel.min.js') }}"></script>
 
 </body>
 </html>
